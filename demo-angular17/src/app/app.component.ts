c8f29e034484c919b130b9e88430e6e088f4a96c
castro2001 <alexandercastrolino@gmail.com>
2024-07-14 12:28:04 -0500

    subiendo cambios a la rama clase1

--- demo-angular17/src/app/app.component.ts
+++ demo-angular17/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LikeComponent } from './like/like.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LikeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

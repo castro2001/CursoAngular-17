f6c3827e9930d39c54b6abf9382e17651b49ee2e
castro2001 <alexandercastrolino@gmail.com>
2024-07-14 12:36:54 -0500

    subiendo cambios de los temas de la clase 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CursoAngular-17
 practica de un sitio web dinamico usando base de datos 
+clase 1
+vimos sobre interpolacion , property binding y event
--- demo-angular17/src/app/like/like.component.ts
+++ demo-angular17/src/app/like/like.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './like.component.css'
 })
 export class LikeComponent {
+  inputMessage="Ingresa tu Nombre";
+  disableButton= false;
+
+  clickEvent():void{
+    alert('click')
+   }
+
+   input(event:Event):void{
+    const inputEvent = event as InputEvent;
+    // console.log('Input...',event)
+    console.log('Input...',inputEvent.data)
+   }
 
 }
